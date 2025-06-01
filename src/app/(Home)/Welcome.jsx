7f9a8b64f749e73e315e7256a239617bb5f48fc8
Nirvik Roy <nirvikroy802@gmail.com>
2025-06-02 00:47:31 +0530

    up

--- src/app/(Home)/Welcome.jsx
+++ src/app/(Home)/Welcome.jsx
@@ -10,43 +10,85 @@ gsap.registerPlugin(ScrollTrigger);
 
 const Welcome = () => {
 
-    useGSAP(()=>{
-        gsap.fromTo('.welcome_img_wrapper',{
-            height:'80%',
-            duration:'1'
-        },{
-            height:'90%',
-            scrollTrigger:{
-                trigger:'.welcome_whole_wrapper',
-                start: 'top 80%',
-                end:'top 0%',
-                scrub:2,
-            
-            }
-        })
-        gsap.fromTo('.welcome_img_wrapper',{
-            height:'90%'
-        },{
-            width:'100%',
-            height:'100%',
-            duration:'1',
-            scrollTrigger:{
-                trigger:'.welcome_whole_wrapper',
-                start:'top -200px',
-                pin:true,
-                scrub:2
-            }
-            
-        })
-    },[])
+  useGSAP(() => {
+    gsap.fromTo('.welcome_img_wrapper', {
+      height: '80%',
+      duration: '1'
+    }, {
+      height: '100%',
+      scrollTrigger: {
+        trigger: '.welcome_whole_wrapper',
+        start: 'top 80%',
+        end: 'top 0%',
+        scrub: 2,
+
+      }
+    })
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.welcome_whole_wrapper',
+        start: 'top -10px',
+        pin: true,
+        scrub: 2,
+      }
+    })
+    tl.fromTo('.welcome_img_wrapper', {
+      height: '100%'
+    }, {
+      width: '100%',
+      height: '100%',
+      duration: '1',
+
+    })
+
+    // tl.to('.welcome_img_wrapper', {
+    //   scale: 1.10,
+    //   scrollTrigger: {
+    //     scrub: 5,
+    //   }
+    // },)
+
+
+    tl.to('.welcome_white_div',{
+      y:0
+    })
+    
+    tl.to('.welcome_text_wrapper',{
+     top:'-100%',
+    
+   
+    },
+
+    )
+
+  }, [])
   return (
     <>
-      <div className='welcome_whole_wrapper'>
-        <div className='welcome_img_wrapper'></div>
+      <div className='welcome_whole_wrapper' style={{
+        position:'relative'
+      }}>
+        <div className='welcome_img_wrapper'>
+
+        </div>
+
+                  <div className='welcome_text_wrapper'>
+            The atmosphere of romantic Venice reigns in our cozy restaurant, where every dish
+            is prepared by hand and with special passion. Just like in old cafes on the narrow streets of a charming city. The chefs skillfully combine fresh and selected ingredients and deliver them to your table with love.
+            We work to ensure that you enjoy the dolce vita and the vibrant flavors of Italy</div>
+
+            <div className='welcome_white_div'>
+              <p>
+Welcome to our sophisticated restaurant in the heart of Venice</p>
+
+
+<h6>Enjoy stunning views of the beautiful city's historic architecture with a
+ glass of fine wine and delicate salami prepared by the world's best chef
+</h6>
+            </div>
       </div>
 
       <div style={{
-        height:'500vh'
+        height: '500vh'
       }}></div>
     </>
   )
